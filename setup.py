1e7796cc15d8c0bae25793f1a4f4e68c4ce7dc7b
vincenzoarcidiacono <vinci1it2000@gmail.com>
2023-11-15 11:16:39 +0100

    feat(core): Add support for python 3.10 and 3.11.

--- setup.py
+++ setup.py
@@ -104,6 +104,8 @@ if __name__ == '__main__':
             "Programming Language :: Python :: 3.7",
             "Programming Language :: Python :: 3.8",
             "Programming Language :: Python :: 3.9",
+            "Programming Language :: Python :: 3.10",
+            "Programming Language :: Python :: 3.11",
             "Programming Language :: Python :: Implementation :: CPython",
             "Development Status :: 5 - Production/Stable",
             'Natural Language :: English',
